cc086e380fdbe79c8f70db1604fbfd8edd3eccc4
LoganRaymond1 <logan@theraymonds.ca>
2025-05-30 13:57:29 -0400

    Create swing.java

---
+++ swing.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class swing{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Swing Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+
+        JButton button = new JButton("Gurt");
+        button.addActionListener(e -> System.out.println("GURTY"));
+        frame.add(button);
+    }
+}
