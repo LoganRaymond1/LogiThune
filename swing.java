b36aa36acd8a4a232448bd04ca60a367fe68411a
tforward52 <thomaswforward@icloud.com>
2025-06-01 13:44:48 -0400

    Login

--- swing.java
+++ swing.java
@@ -1,15 +1,50 @@
 import javax.swing.*;
 
-public class swing{
+public class Swing {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Swing Test");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Login login = new Login();
+                login.show(); 
+            }
+        });
+    }
+}
 
-        JButton button = new JButton("Gurt");
-        button.addActionListener(e -> System.out.println("GURTY"));
-        frame.add(button);
-        
-        frame.setVisible(true);
+class Login {
+    private JFrame loginFrame;
+
+    public Login() {
+        loginFrame = new JFrame("Login"); 
+
+        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginFrame.setSize(400, 300);
+        loginFrame.setLocationRelativeTo(null);
+        loginFrame.setLayout(null);
+
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setBounds(50, 50, 100, 30);
+        loginFrame.add(usernameLabel);
+
+        JTextField usernameField = new JTextField();
+        usernameField.setBounds(150, 50, 200, 30);
+        loginFrame.add(usernameField);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(50, 100, 100, 30);
+        loginFrame.add(passwordLabel);
+
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(150, 100, 200, 30);
+        loginFrame.add(passwordField);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(150, 150, 100, 30);
+        loginFrame.add(loginButton);
+    }
+
+    public void show() {
+        loginFrame.setVisible(true);
     }
 }
